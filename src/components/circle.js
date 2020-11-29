8a4a3d2b1bba955c89bd928668aa62fdd0683c07
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-11-29 21:03:27 +0530

    Changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
 
+const { default: Shape } = require("./shape");
+
+//const { default: Shape } = require("./shape");
+
 // declare class
 
-
 // export class using module.exports
+
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea = function () {};
+}
+export default Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
 
 // export class using module.exports
+
+export default class Shape {
+  color = null;
+
+  constructor(color) {
+    this.color = color;
+  }
+
+  drawShape = function () {};
+  calculateArea = function () {};
+}
